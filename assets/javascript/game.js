f3082c06c28afce15c720382a97d5878ce13a699
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-21 10:20:12 -0400

    alls working except for reloading game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,31 @@
 let computerNumber = randomIntBetween(19, 120);
 
 let gemNumbers = ["_", "_", "_", "_"];
-for (let i = 0; i < gemNumbers.length; i++) {
-    gemNumbers[i] = randomIntBetween(1, 12);
-}
+// for (let i = 0; i < gemNumbers.length; i++) {
+//     gemNumbers[i] = randomIntBetween(1, 12);
+// }
 let userScore = 0;
 let win = 0;
 let loss = 0;
 // set up ids
-for (let i = 0; i < gemNumbers.length; i++) {
-    const gem = $("<div>");
-    gem.addClass("gem");
-    gem.attr("data-rand-num", gemNumbers[i]);
-    $("#gem-holder").append(gem);
-}
 
-console.log(computerNumber);
+setUp();
+
 //on click
 $("#win-holder").text(win);
 $("#loss-holder").text(loss);
 $("#target-number").text(computerNumber);
 
+
+
 $(".gem").on("click", function() {
     userScore += parseInt($(this).attr("data-rand-num"));
-    console.log(userScore);
+
     compare(userScore, computerNumber);
     $("#score").text(userScore);
+
 });
 
-
-
-// $(document).ready(function() {
-//     gemOne.click(function() {
-//         userScore += gemNumbers[0];
-//         compare(userScore, computerNumber)
-//         console.log(userScore);
-//     });
-
-
-
-
-//add to players score
-// if playerscore === randomnumber
-// win++
-//win
-//else playerscore > randomnumber
-// loss++
-//loss
-
-//output wins losses
 
 //functions
 function randomIntBetween(min, max) {
@@ -61,13 +38,31 @@ function compare(userNum, compNum) {
         console.log("you win");
         win++;
         $("#win-holder").text(win);
-
-
+        $("#gem-holder").empty();
+        setUp();
 
     } else if (userNum > compNum) {
         console.log("you lose");
         loss++;
         $("#loss-holder").text(loss);
+        $("#gem-holder").empty();
+        setUp();
 
     } else {}
 }
+
+function setUp() {
+    computerNumber = randomIntBetween(19, 120);
+    userScore = 0
+
+    for (let i = 0; i < gemNumbers.length; i++) {
+        gemNumbers[i] = randomIntBetween(1, 12);
+    }
+
+    for (let i = 0; i < gemNumbers.length; i++) {
+        const gem = $("<div>");
+        gem.addClass("gem");
+        gem.attr("data-rand-num", gemNumbers[i]);
+        $("#gem-holder").append(gem);
+    }
+};
