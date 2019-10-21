f9019b473d01277543af93838a368eb928394ae4
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-21 13:05:38 -0400

    all logic seems to be working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,16 @@
 let computerNumber = randomIntBetween(19, 120);
-
 let gemNumbers = ["_", "_", "_", "_"];
-// for (let i = 0; i < gemNumbers.length; i++) {
-//     gemNumbers[i] = randomIntBetween(1, 12);
-// }
 let userScore = 0;
 let win = 0;
 let loss = 0;
-// set up ids
 
+const winHolder = $("#win-holder").text(win);
+const lossHolder = $("#loss-holder").text(loss);
+const targetNumber = $("#target-number").text(computerNumber);
 setUp();
 
-//on click
-$("#win-holder").text(win);
-$("#loss-holder").text(loss);
-$("#target-number").text(computerNumber);
 
 
-
-$(".gem").on("click", function() {
-    userScore += parseInt($(this).attr("data-rand-num"));
-
-    compare(userScore, computerNumber);
-    $("#score").text(userScore);
-
-});
 
 
 //functions
@@ -33,26 +19,9 @@ function randomIntBetween(min, max) {
     return randNum;
 }
 
-function compare(userNum, compNum) {
-    if (userNum === compNum) {
-        console.log("you win");
-        win++;
-        $("#win-holder").text(win);
-        $("#gem-holder").empty();
-        setUp();
-
-    } else if (userNum > compNum) {
-        console.log("you lose");
-        loss++;
-        $("#loss-holder").text(loss);
-        $("#gem-holder").empty();
-        setUp();
-
-    } else {}
-}
-
 function setUp() {
     computerNumber = randomIntBetween(19, 120);
+    targetNumber.text(computerNumber);
     userScore = 0
 
     for (let i = 0; i < gemNumbers.length; i++) {
@@ -65,4 +34,30 @@ function setUp() {
         gem.attr("data-rand-num", gemNumbers[i]);
         $("#gem-holder").append(gem);
     }
-};
+
+    $(".gem").on("click", function() {
+        userScore += parseInt($(this).attr("data-rand-num"));
+
+        compare(userScore, computerNumber);
+        $("#score").text(userScore);
+
+    });
+}
+
+function compare(userNum, compNum) {
+    if (userNum === compNum) {
+        console.log("you win");
+        win++;
+        winHolder.text(win);
+        $("#gem-holder").empty();
+        setUp();
+
+    } else if (userNum > compNum) {
+        console.log("you lose");
+        loss++;
+        lossHolder.text(loss);
+        $("#gem-holder").empty();
+        setUp();
+
+    } else {}
+}
