4eacae592689ed6704fa03085d23ca283520d1bd
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-21 09:31:01 -0400

    basic logic working, basic displays working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,55 @@
-let computerNumber = randomIntBetween(25, 50);
+let computerNumber = randomIntBetween(19, 120);
 
-let gemNumbers = ["_", "_", "_", "-"];
+let gemNumbers = ["_", "_", "_", "_"];
 for (let i = 0; i < gemNumbers.length; i++) {
-    gemNumbers[i] = randomIntBetween(1, 10);
+    gemNumbers[i] = randomIntBetween(1, 12);
 }
 let userScore = 0;
 let win = 0;
 let loss = 0;
 // set up ids
-const gemOne = $("#gem-1");
-const gemTwo = $("#gem-2");
-const gemThree = $("#gem-3");
-const gemFour = $("#gem-4");
+for (let i = 0; i < gemNumbers.length; i++) {
+    const gem = $("<div>");
+    gem.addClass("gem");
+    gem.attr("data-rand-num", gemNumbers[i]);
+    $("#gem-holder").append(gem);
+}
+
 console.log(computerNumber);
 //on click
-$(document).ready(function() {
-    gemOne.click(function() {
-        userScore += gemNumbers[0];
-        compare(userScore, computerNumber)
-        console.log(userScore);
-    });
+$("#win-holder").text(win);
+$("#loss-holder").text(loss);
+$("#target-number").text(computerNumber);
 
-    gemTwo.click(function() {
-        userScore += gemNumbers[1];
-        compare(userScore, computerNumber)
-        console.log(userScore);
-    });
-
-    gemThree.click(function() {
-        userScore += gemNumbers[2];
-        compare(userScore, computerNumber)
-        console.log(userScore);
-    });
-
-    gemFour.click(function() {
-        userScore += gemNumbers[3];
-        compare(userScore, computerNumber)
-        console.log(userScore);
-    });
-
-
-    //add to players score
-    // if playerscore === randomnumber
-    // win++
-    //win
-    //else playerscore > randomnumber
-    // loss++
-    //loss
-
-    //output wins losses
+$(".gem").on("click", function() {
+    userScore += parseInt($(this).attr("data-rand-num"));
+    console.log(userScore);
+    compare(userScore, computerNumber);
+    $("#score").text(userScore);
 });
+
+
+
+// $(document).ready(function() {
+//     gemOne.click(function() {
+//         userScore += gemNumbers[0];
+//         compare(userScore, computerNumber)
+//         console.log(userScore);
+//     });
+
+
+
+
+//add to players score
+// if playerscore === randomnumber
+// win++
+//win
+//else playerscore > randomnumber
+// loss++
+//loss
+
+//output wins losses
+
 //functions
 function randomIntBetween(min, max) {
     let randNum = Math.floor(Math.random() * ((max - 1) - min + 1)) + min;
@@ -59,7 +59,15 @@ function randomIntBetween(min, max) {
 function compare(userNum, compNum) {
     if (userNum === compNum) {
         console.log("you win");
+        win++;
+        $("#win-holder").text(win);
+
+
+
     } else if (userNum > compNum) {
         console.log("you lose");
+        loss++;
+        $("#loss-holder").text(loss);
+
     } else {}
 }
