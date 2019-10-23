7ac4067e69b72bbad3f48d111c7134edec762b72
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-23 10:33:48 -0400

    made the values appear after the value is clicked for the first time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ let loss = 0;
 const winHolder = $("#win-holder").text(win);
 const lossHolder = $("#loss-holder").text(loss);
 const targetNumber = $("#target-number").text(computerNumber);
+const valueHolder = $("#value-holder").text("???");
 setUp();
 
 
@@ -32,9 +33,11 @@ function setUp() {
         const gem = $("<div>");
         gem.addClass("gem");
         gem.addClass("gem" + randomIntBetween(1, 5));
+        gem.append('<div id="value' + [i] + '" class="value hidden">' + gemNumbers[i] + '</div>');
 
         gem.attr("data-rand-num", gemNumbers[i]);
         $("#gem-holder" + i).append(gem);
+
 
 
 
@@ -43,9 +46,10 @@ function setUp() {
 
     $(".gem").on("click", function() {
         userScore += parseInt($(this).attr("data-rand-num"));
-
+        $("div", this).removeClass("hidden")
         compare(userScore, computerNumber);
         $("#score").text(userScore);
+
 
     });
 }
@@ -69,29 +73,11 @@ function compare(userNum, compNum) {
 }
 
 function addStyle() {
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 7; i++) {
         const part = $("<div>");
         part.addClass("part" + i);
         $(".gem").append(part);
     }
 
-    // const part2 = $("<div>");
-    // part2.addClass("part2");
-    // $(".gem").append(part2);
 
-    // const part3 = $("<div>");
-    // part3.addClass("part3");
-    // $(".gem").append(part3);
-
-    // const part4 = $("<div>");
-    // part4.addClass("part4");
-    // $(".gem").append(part4);
-
-    // const part5 = $("<div>");
-    // part5.addClass("part5");
-    // $(".gem").append(part5);
-
-    // const part6 = $("<div>");
-    // part6.addClass("part6");
-    // $(".gem").append(part6);
 }
